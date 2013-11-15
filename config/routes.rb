e0aee29ab05e6e44726e4aec30a790814308c5eb
K C <aries0420@gmail.com>
2013-11-15 00:10:48 -0800

    Customized initial front-end work (including background images, icons, as well as some initial test.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Myvoice::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get '/about', to: 'static#about'
+  get '/contact', to: 'static#contact'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,15 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+require "minitest/focus"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
+require "minitest/rails/capybara"
 
 # Uncomment for awesome colorful output
-# require "minitest/pride"
+require "minitest/pride"
+require "minitest/colorize"
 
 class ActiveSupport::TestCase
     ActiveRecord::Migration.check_pending!
