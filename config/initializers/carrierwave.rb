b650ed1360e0a93ad72603bb2f290241ac880e82
K C <aries0420@gmail.com>
2013-11-19 23:47:50 -0800

    Added local timestamp and i18n supports. Title translation issue needs review again. Added browser-timezone-rails gem for detecting local timezone automatically.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider: "AWS",
-    # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-    aws_access_key_id: 'AKIAJVE7UYLEHIJZIYUQ',
-    aws_secret_access_key: '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'
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
   }
-  # config.fog_directory = ENV["AWS_S3_BUCKET"]
-  config.fog_directory = 'myvoicekc'
+  config.fog_directory = ENV["AWS_S3_BUCKET"]
 end
